767f2e1a3e1344be4e072cf541df41228180d855
Armaghan Bin Zahid <armaghanbinzahid@gmail.com>
2023-02-11 21:58:52 +0500

    Add files via upload

--- NavMesh.cs
+++ NavMesh.cs
@@ -1,4 +1,3 @@
-using Dreamteck.Splines;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,15 +7,19 @@ public class NavMesh : MonoBehaviour
 {
     [SerializeField] private Transform transformPosition;
     private NavMeshAgent navMesh;
+    //Animator anime;
     private void Awake()
     {
         navMesh = GetComponent<NavMeshAgent>();
+        //anime= GetComponent<Animator>();
     }
     private void Update()
     {
         if (navMesh.enabled == true)
         {
+            //anime.SetBool("isWalking", true);
             navMesh.destination = transformPosition.position;
+            
         }
     }
 }
