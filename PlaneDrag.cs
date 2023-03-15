6ec6ebb30d680efc430effc5ae437947cd39b064
Armaghan Bin Zahid <armaghanbinzahid@gmail.com>
2023-03-15 17:08:04 +0500

    Add files via upload

---
+++ PlaneDrag.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneDrag : MonoBehaviour
+{
+    public float dragValue;
+    
+
+    private void Start()
+    {
+    }
+
+
+}
